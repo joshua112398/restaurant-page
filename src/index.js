9b61f91590a3dc63d168b29d8c201d4e6e64f6e9
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-06-12 04:44:51 -0700

    Design main content layout
    Create the main content portion of the website, having slots for
    an image and a caption next to it. Also, make it responsive to
    the browser size and mobile browsers. In addition, make it flexible
    for adding more information or removing information.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import normalize from "normalize.css";
 import css from "./style.css";
 import bg from "./kirsten-drew-n49BjsFf5dI-unsplash.jpg"
 import bread from "./food-svgrepo-com.svg"
+import croissant from "./conor-brown-sqkXyyj4WdE-unsplash.jpg"
 
 // Manage loading of the homepage's different elements
 const loadHomepage = (() => {
