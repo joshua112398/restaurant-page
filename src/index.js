4b98bff5e27d9c9ff7e2d891dd1c23ab907c55ba
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-06-11 19:34:53 -0700

    Complete design of website header
    Create a header background image and include it in the webpack
    bundle. Also finish the navigation bar of the page.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hey");
+import normalize from "normalize.css";
+import css from "./style.css";
+import bg from "./kirsten-drew-n49BjsFf5dI-unsplash.jpg"
+import bread from "./food-svgrepo-com.svg"
+
+// Manage loading of the homepage's different elements
+const loadHomepage = (() => {
+
+})();
+
+const loadHero = function() {
+
+}
