a9872ebca3423798cab47be2304891a3e821485d
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-06-12 05:27:01 -0700

    Generate header using JavaScript
    Header is now created entirely using JavaScript instead of a base
    HTML file.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,44 @@ import bread from "./food-svgrepo-com.svg"
 import croissant from "./conor-brown-sqkXyyj4WdE-unsplash.jpg"
 
 // Manage loading of the homepage's different elements
-const loadHomepage = (() => {
+const Loader = (() => {
 
+  const content = document.querySelector("#content");
+  const hero = document.createElement("div");
+
+  const setup = function() {
+    hero.classList.add("hero");
+    content.appendChild(hero);
+    console.log("Loader");
+    Header.load();
+  }
+
+  return {setup, hero};
 })();
 
-const loadHero = function() {
+const Header = (() =>{
 
-}
+  const load = function() {
+    const img = document.createElement("img");
+    img.setAttribute("src", bread);
+    img.classList.add("logo");
+    const header = document.createElement("div");
+    header.classList.add("header");
+    Loader.hero.appendChild(img);
+    Loader.hero.appendChild(header);
+  }
+
+  return {load};
+})();
+
+const Homepage = function() {
+
+  const load = function() {
+    const body = document.querySelector("#content");
+
+  }
+
+  return {load};
+};
+
+Loader.setup();
